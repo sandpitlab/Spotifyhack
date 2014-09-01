c40ae9793c5706af72dd67b004a73f97a4538766
Mateusz Zatorski <mateuszzatorski@gmail.com>
2014-09-01 13:51:19 +0100

    don't add playlist if the ID is null (that means it's a starred playlist)

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,9 @@ module.exports = function(app, passport) {
 	// handle the callback after spotify has authenticated the user
 	app.get('/auth/spotify/callback',
 		passport.authenticate('spotify', {
-      successRedirect : '/profile',
+			successRedirect : '/profile',
 			failureRedirect : '/'
-		}));
+		}), function(req, res) {
+			log('HELLO!')
+		});
 };
